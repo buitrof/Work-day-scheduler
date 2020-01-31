19cef87f345bde96b077451dc718ae4f0f61f0ca
fbuitron <franciscobuitron21@gmail.com>
2020-01-30 22:47:03 -0800

    added rows and some styling

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,1 +1,9 @@
 $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'))
+
+// if (currentTime = moment().format(whatever)) {
+//   time.classList('present')
+// } else if (current time = before moment().format(what)) {
+//   time.classList('past')
+// } else {
+//   time.classList('future')
+// }
